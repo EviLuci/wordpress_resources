eed06e3a8ef5f92bcc7bd1aa6f27de6104d6a2cc
EviLuci <sujan.koju111@gmail.com>
2025-05-16 23:04:22 +0545

    child theme

---
+++ child theme/admin/customize_admin.php
@@ -0,0 +1,8 @@
+<?php
+
+// Dashboard Customization
+require_once get_stylesheet_directory() . '/admin/dashboard/disable_admin_notices.php';
+require_once get_stylesheet_directory() . '/admin/dashboard/disable.php';
+
+// Login Customization
+// require_once get_stylesheet_directory() . '/admin/login/customize_login.php';
